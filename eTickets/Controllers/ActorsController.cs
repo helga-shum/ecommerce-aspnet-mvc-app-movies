1b5cb7c45981022c2a61dff05aec5f0b63442f32
helga-shum <shumilova.olha@gmail.com>
2023-01-12 16:42:15 +0100

    Adding ActorsController

---
+++ eTickets/Controllers/ActorsController.cs
@@ -0,0 +1,19 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDBContext _context;
+        public ActorsController(AppDBContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
