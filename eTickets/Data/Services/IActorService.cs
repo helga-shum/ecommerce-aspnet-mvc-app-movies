b56703ea7a5e793d3f5b079d08b4d208537312ba
helga-shum <shumilova.olha@gmail.com>
2023-01-12 19:36:44 +0100

    Adding IActorService interface

---
+++ eTickets/Data/Services/IActorService.cs
@@ -0,0 +1,14 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Services
+{
+    public interface IActorService
+    {
+        IEnumerable<Actor> GetAll();
+        Actor GetBy(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+        void Delete(int id);
+
+    }
+}
